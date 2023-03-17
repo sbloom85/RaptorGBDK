bb24151cb675a7574f81f30585bb30b289aa519f
sbloom85 <sbloom85@gmail.com>
2023-03-16 21:31:46 -0500

    Sprite Movement
    Some Sprite movement.

--- main.c
+++ main.c
@@ -4,8 +4,33 @@
 
 void main(void)
 {
-    set_sprite_data(0, 2, Smiley);
+    uint8_t tileSize = 8;
+    set_sprite_data(0, 4, Smiley);
     set_sprite_tile(0, 0);
-    move_sprite(0, 88, 78);
+    move_sprite(0, 80, 72);
     SHOW_SPRITES;
+
+    while(1)
+    {
+        switch(joypad())
+        {
+            case J_LEFT:
+                scroll_sprite(0, -tileSize, 0);
+                set_sprite_tile(0, 0);
+                break;
+            case J_RIGHT:
+                scroll_sprite(0, tileSize, 0);
+                set_sprite_tile(0, 3);
+                break;
+            case J_UP:
+                scroll_sprite(0, 0, -tileSize);
+                set_sprite_tile(0, 2);
+                break;
+            case J_DOWN:
+                scroll_sprite(0, 0, tileSize);
+                set_sprite_tile(0, 1);
+                break;
+        }
+        delay(100);
+    }
 }
