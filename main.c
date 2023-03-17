7749a7981a64eb9ffc2846f224f3463e8ea30af7
sbloom85 <sbloom85@gmail.com>
2023-03-16 20:50:26 -0500

    Smiley Sprite
    Showing a smiley sprite and fixed the make batch file.

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <gb/gb.h>
 #include <stdio.h>
+#include "sprites/Smiley.c"
 
 void main(void)
 {
-    printf("Hello World");
+    set_sprite_data(0, 2, Smiley);
+    set_sprite_tile(0, 0);
+    move_sprite(0, 88, 78);
+    SHOW_SPRITES;
 }
--- make.bat
+++ make.bat
@@ -1,2 +1,2 @@
 c:\libraries\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o main.o main.c
-c:\libraries\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o main.gb out/main.o
+c:\libraries\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o main.gb main.o
