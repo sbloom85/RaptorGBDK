1ed6149ea961290711a75d15181b84214d03e7be
sbloom85 <sbloom85@gmail.com>
2023-03-17 08:41:19 -0500

    Stuff
    Background scrolling stuff.

--- main.c
+++ main.c
@@ -1,36 +1,19 @@
 #include <gb/gb.h>
 #include <stdio.h>
-#include "sprites/Smiley.c"
+#include "maps/BackTiles.c"
+#include "maps/SimpleBackground.c"
 
 void main(void)
 {
-    uint8_t tileSize = 8;
-    set_sprite_data(0, 4, Smiley);
-    set_sprite_tile(0, 0);
-    move_sprite(0, 80, 72);
-    SHOW_SPRITES;
+    set_bkg_data(0, 7, BackTiles);
+    set_bkg_tiles(0, 0, 40, 18, SimpleBackground);
 
-    while(1)
+    SHOW_BKG;
+    DISPLAY_ON;
+
+    while (1)
     {
-        switch(joypad())
-        {
-            case J_LEFT:
-                scroll_sprite(0, -tileSize, 0);
-                set_sprite_tile(0, 0);
-                break;
-            case J_RIGHT:
-                scroll_sprite(0, tileSize, 0);
-                set_sprite_tile(0, 3);
-                break;
-            case J_UP:
-                scroll_sprite(0, 0, -tileSize);
-                set_sprite_tile(0, 2);
-                break;
-            case J_DOWN:
-                scroll_sprite(0, 0, tileSize);
-                set_sprite_tile(0, 1);
-                break;
-        }
+        scroll_bkg(1, 0);
         delay(100);
     }
 }
