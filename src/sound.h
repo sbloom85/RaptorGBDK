c6ec027fc6a1be089d38f2c4249c5baaaa09096f
sbloom85 <sbloom85@gmail.com>
2023-03-18 12:28:50 -0500

    More Audio Stuff

--- src/sound.h
+++ src/sound.h
@@ -1,10 +1,12 @@
 #include "gb.h"
+#include "hUGEDriver.h"
+#include <stddef.h>
 
 void InitializeSound()
 {
     NR52_REG = 0X80;    // Is 1000 0000 in binary, turns on sound
-    NR50_REG = 0X77;    // Sets the volume for both left and right channel
     NR51_REG = 0XFF;    // Selects which channels we want to use
+    NR50_REG = 0X77;    // Sets the volume for both left and right channel
 }
 
 void TutorialSound()
@@ -64,6 +66,11 @@ void HitSound()
 void DeadSound()
 {
 
+}
+
+void TutorialMusic()
+{
+    
 }
 
 // Plays when the title screen is displayed
