f6bdb8904aa85059bab2e559d68e37027f65966f
sbloom85 <sbloom85@gmail.com>
2023-03-19 09:25:35 -0500

    Make Changes
    Changed output to raptor.gbc

--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
 mkdir -p build
 c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o build\main.o src\main.c
 c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o build\raptor.o sprites\raptor.c
-c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o main.gb build\main.o build\raptor.o
+c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o raptor.gbc build\main.o build\raptor.o
 pause
