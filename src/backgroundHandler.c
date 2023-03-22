6682f1440a26a089d124a1d43221ba4811662499
Devin <70994866+RetroGamer02@users.noreply.github.com>
2023-03-22 11:23:44 -0400

    Edits to Update HUD Still WIP

--- src/backgroundHandler.c
+++ src/backgroundHandler.c
@@ -44,7 +44,6 @@ const UWORD bkgPalette[] = {
     RaptorMapTilesCGBPal7c0, RaptorMapTilesCGBPal7c1, RaptorMapTilesCGBPal7c2, RaptorMapTilesCGBPal7c3,
 };
 
-//Add logic to update Hud here.
 unsigned char RaptorWindowUpdatePLN0[] =
 {
   0x75,0x75,0x75,0x75,0x48,0x49,0x49,0x49,0x49,0x49,
@@ -71,12 +70,32 @@ void SGBTransferPalettes(const UWORD *SGBPallete) BANKED {
     sgb_transfer((void *)&data);
 }
 
+struct windowDataStruct {
+    uint8_t CashP1:4,
+            CashP2:4;
+    uint8_t CashP3:4,
+            CashP4:4;
+    uint8_t CashP5:4,
+            CashP6:4;
+    uint8_t CashP7:4,
+            CashP8:4;
+    uint8_t CashP9:4;
+
+    uint8_t HlthP1:4,
+            HlthP2:4;
+    uint8_t HlthP3:4;
+
+    uint8_t ShldP1:4,
+            ShldP2:4;
+    uint8_t ShldP3:4;
+} windowData;
+
 void updateHud()
 {
-    for (uint8_t i = 0; i < 40; i++)
+    for (uint8_t i = 40; i--;)
     {
         //Line 1
-        if (i < 4 || i > 14 && i < 20) //Empty Space line 1
+        if (i < 4 || i > 14 && i < 20) //Empty Space
         {
             RaptorWindowUpdatePLN0[i] = 0x75;
         }
@@ -86,43 +105,43 @@ void updateHud()
         }
         if (i == 5) //Cash Pos 1
         {
-            RaptorWindowUpdatePLN0[i] = 0x49 + 1;
+            RaptorWindowUpdatePLN0[i] = 0x49 + windowData.CashP1;
         }
         if (i == 6) //Cash Pos 2
         {
-            RaptorWindowUpdatePLN0[i] = 0x49;
+            RaptorWindowUpdatePLN0[i] = 0x49 + windowData.CashP2;
         }
         if (i == 7) //Cash Pos 3
         {
-            RaptorWindowUpdatePLN0[i] = 0x49;
+            RaptorWindowUpdatePLN0[i] = 0x49 + windowData.CashP3;
         }
         if (i == 8) //Cash Pos 4
         {
-            RaptorWindowUpdatePLN0[i] = 0x49;
+            RaptorWindowUpdatePLN0[i] = 0x49 + windowData.CashP4;
         }
         if (i == 9) //Cash Pos 5
         {
-            RaptorWindowUpdatePLN0[i] = 0x49;
+            RaptorWindowUpdatePLN0[i] = 0x49 + windowData.CashP5;
         }
         if (i == 10) //Cash Pos 6
         {
-            RaptorWindowUpdatePLN0[i] = 0x49;
+            RaptorWindowUpdatePLN0[i] = 0x49 + windowData.CashP6;
         }
         if (i == 11) //Cash Pos 7
         {
-            RaptorWindowUpdatePLN0[i] = 0x49;
+            RaptorWindowUpdatePLN0[i] = 0x49 + windowData.CashP7;
         }
         if (i == 12) //Cash Pos 8
         {
-            RaptorWindowUpdatePLN0[i] = 0x49;
+            RaptorWindowUpdatePLN0[i] = 0x49 + windowData.CashP8;
         }
         if (i == 13) //Cash Pos 9
         {
-            RaptorWindowUpdatePLN0[i] = 0x49;
+            RaptorWindowUpdatePLN0[i] = 0x49 + windowData.CashP9;
         }
         
         //Line 2
-        if (i == 20 || i == 25 || i == 30 || i == 36 || i == 39) //Empty space
+        if (i == 20 || i == 25 || i == 30 || i == 36 || i == 39) //Empty Space
         {
             RaptorWindowUpdatePLN0[i] = 0x75;
         }
@@ -132,15 +151,15 @@ void updateHud()
         }
         if (i == 22) //Health Pos 1
         {
-            RaptorWindowUpdatePLN0[i] = 0x49 + 1;
+            RaptorWindowUpdatePLN0[i] = 0x49 + windowData.HlthP1;
         }
         if (i == 23) //Health Pos 2
         {
-            RaptorWindowUpdatePLN0[i] = 0x49;
+            RaptorWindowUpdatePLN0[i] = 0x49 + windowData.HlthP2;
         }
         if (i == 24) //Health Pos 3
         {
-            RaptorWindowUpdatePLN0[i] = 0x49;
+            RaptorWindowUpdatePLN0[i] = 0x49 + windowData.HlthP3;
         }
         if (i == 26) //S Char
         {
@@ -148,15 +167,15 @@ void updateHud()
         }
         if (i == 27) //Shield Pos 1
         {
-            RaptorWindowUpdatePLN0[i] = 0x49;
+            RaptorWindowUpdatePLN0[i] = 0x49  + windowData.ShldP1;
         }
         if (i == 28) //Shield Pos 2
         {
-            RaptorWindowUpdatePLN0[i] = 0x49;
+            RaptorWindowUpdatePLN0[i] = 0x49  + windowData.ShldP2;
         }
         if (i == 29) //Shield Pos 3
         {
-            RaptorWindowUpdatePLN0[i] = 0x49;
+            RaptorWindowUpdatePLN0[i] = 0x49  + windowData.ShldP3;
         }
         if (i == 31) //Shield Icon Pos 1
         {
