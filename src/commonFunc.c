24153673026014fccaf3021707e86d5ef4cffc14
Devin <70994866+RetroGamer02@users.noreply.github.com>
2023-04-05 06:45:08 -0400

    Update commonFunc.c

--- src/commonFunc.c
+++ src/commonFunc.c
@@ -1,8 +1,14 @@
 #pragma bank 0
 
 #include "gb.h"
+#include "sgb.h"
 #include "cgb.h"
 #include "commonFunc.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <string.h>
 
 static const uint16_t fadeout_palette[] = {RGB_WHITE, RGB_LIGHTGRAY,  RGB_DARKGRAY, RGB_BLACK, RGB_BLACK, RGB_BLACK, RGB_BLACK};
 
@@ -13,6 +19,53 @@ void PerformantDelay(uint8_t numLoops)
     {
         wait_vbl_done();
     }
+}
+
+uint8_t getGBType()
+{
+    switch (_cpu) {
+	case 0x01:
+        if (sgb_check())
+        {
+            return 2; //SGB1
+        } else {
+            return 1; //DMG or MEGADUCK
+        }
+		break;
+	case 0xFF:
+		if (sgb_check())
+        {
+            return 4; //SGB2
+        } else {
+            return 3; //POCKET
+        }
+		break;
+	case 0x11:
+        if (_is_GBA)
+        {
+            return 6; //GBA
+        } else {
+            return 5; //COLOR
+        }
+		break;
+	default:
+		return 0; //OTHER
+		break;
+	}
+}
+
+//Thanks to GB Studio which the SGB code is based off.
+typedef struct sgb_pal_packet_t {
+    UBYTE cmd;
+    UWORD palettes[7];
+} sgb_pal_packet_t;
+
+void SGBTransferPalettes(const UWORD *SGBPallete) NONBANKED
+{
+    sgb_pal_packet_t data;
+    data.cmd = (SGB_PAL_01 << 3) | 1;
+    memcpy(data.palettes, &SGBPallete[0], sizeof(data.palettes));
+    sgb_transfer((void *)&data);
 }
 
 void fadein() NONBANKED
@@ -41,4 +94,15 @@ void fadeout() NONBANKED
     }
 
     set_bkg_tiles(0, 0, 20, 18, BlckScrTiles);
+}
+
+//Thanks to DAN COX for the collison code.
+uint8_t collisionCheck(uint8_t x1, uint8_t y1, uint8_t w1, uint8_t h1, uint8_t x2, uint8_t y2, uint8_t w2, uint8_t h2) 
+{
+	if ((x1 < (x2+w2)) && ((x1+w1) > x2) && (y1 < (h2+y2)) && ((y1+h1) > y2)) 
+    {
+		return 1;
+	} else {
+		return 0;
+	}
 }
