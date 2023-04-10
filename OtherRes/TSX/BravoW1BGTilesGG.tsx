f78a1c1b1c2c45acc38d628d32df7fcbe216b3a1
Devin <70994866+RetroGamer02@users.noreply.github.com>
2023-04-10 14:27:15 -0400

    Upload GG Resources

---
+++ OtherRes/TSX/BravoW1BGTilesGG.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="BravoW1BGTilesGG" tilewidth="8" tileheight="8" tilecount="192" columns="16">
+ <image source="BravoW1BGTilesGG.png" width="128" height="96"/>
+</tileset>
