4fddc8f3dc06cd9a87643ec62ed32eb7d61bd53b
Devin <70994866+RetroGamer02@users.noreply.github.com>
2023-04-20 15:19:58 -0400

    Add some GPL2 stuff to source files

--- src/gameSectionsCommon.h
+++ src/gameSectionsCommon.h
@@ -1,3 +1,28 @@
+/*-----------------------------------------------------------------------------
+ *
+ *  RaptorGB: a Gameboy Demake based off the MSDOS Game 
+ *  Raptor: Call of the Shadows
+ *  Copyright (C) 1994 by
+ *  Apogee Software, Scott Host, Rich Fleider, Tim Neveu,  
+ *  Matt Murphy, Robert Prince, Les Dorscheid, Steven Maines
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ *  02111-1307, USA.
+ *
+ *-----------------------------------------------------------------------------*/
+
 #ifndef GAMESECTC_H_INCLUDE
 #define GAMESECTC_H_INCLUDE
 #ifdef __TARGET_gb
